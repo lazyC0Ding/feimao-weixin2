e5feb2e7011ec8cf33e62358c9d9e635e3af591f
陈松麟 <527935396@qq.com>
2018-06-23 10:36:33 +0800

    修改文章详情图片bug

--- src/pages/article_detail/App.vue
+++ src/pages/article_detail/App.vue
@@ -671,10 +671,10 @@
           for(let i of this.content.article.content){
             if(i.type === "image"){
               if(i.content.indexOf(".null") >=0){
-                i.content.replace(".null","?imageView2/0/format/jpg");
+                i.content = i.content.replace(".null","?imageView2/0/format/jpg");
               }
               if(i.content.indexOf(".webp") >=0){
-                i.content.replace(".webp","?imageView2/0/format/jpg");
+                i.content = i.content.replace(".webp","?imageView2/0/format/jpg");
               }
             }
           }
